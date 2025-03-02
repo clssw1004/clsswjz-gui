cc91ca0ae755ab896ba3c10c130592018432ca8e
cuiwei <838133633@qq.com>
2025-03-02 13:44:17 +0800

    chore: bump version to 1.0.0-alpha.4 | 更新版本号至 1.0.0-alpha.4

--- lib/pages/settings/about_page.dart
+++ lib/pages/settings/about_page.dart
@@ -75,7 +75,7 @@ class AboutPage extends StatelessWidget {
                     ),
                   ),
                   child: Text(
-                    '${L10nManager.l10n.version}: 1.0.0-alpha.3',
+                    '${L10nManager.l10n.version}: 1.0.0-alpha.4',
                     style: theme.textTheme.bodyMedium?.copyWith(
                       color: colorScheme.onSurfaceVariant,
                     ),
