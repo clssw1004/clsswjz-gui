7e6853cbc41b99c1feb84aa6b520d89b49ac30da
cuiwei <838133633@qq.com>
2025-03-12 20:43:31 +0800

    upgrade to v1.0.0-alpha.11

--- lib/utils/file_util.dart
+++ lib/utils/file_util.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:open_filex/open_filex.dart';
 import 'package:path_provider/path_provider.dart';
 // import 'package:open_filex/open_filex.dart';
 import 'package:path/path.dart' as path;
@@ -29,26 +30,26 @@ class FileUtil {
         return OperateResult.failWithMessage(message: '文件不存在');
       }
 
-      // File file = await toCacheFile(attachment);
-      // final result = await OpenFilex.open(file.path);
+      File file = await toCacheFile(attachment);
+      final result = await OpenFilex.open(file.path);
 
-      // if (result.type == ResultType.error) {
-      //   return OperateResult.failWithMessage(
-      //     message: '打开文件失败：${result.message}',
-      //   );
-      // }
+      if (result.type == ResultType.error) {
+        return OperateResult.failWithMessage(
+          message: '打开文件失败：${result.message}',
+        );
+      }
 
-      // if (result.type == ResultType.noAppToOpen) {
-      //   return OperateResult.failWithMessage(
-      //     message: '没有找到可以打开该类型文件的应用',
-      //   );
-      // }
+      if (result.type == ResultType.noAppToOpen) {
+        return OperateResult.failWithMessage(
+          message: '没有找到可以打开该类型文件的应用',
+        );
+      }
 
-      // if (result.type == ResultType.permissionDenied) {
-      //   return OperateResult.failWithMessage(
-      //     message: '没有权限打开文件',
-      //   );
-      // }
+      if (result.type == ResultType.permissionDenied) {
+        return OperateResult.failWithMessage(
+          message: '没有权限打开文件',
+        );
+      }
 
       return OperateResult.success(null);
     } catch (e) {
